4ab35cdcf0357735a12140466063663e5ead122f
lokeshvenkatesan2704 <lokeshvenkatesan2704@gmail.com>
2026-02-24 06:30:26 +0530

    UC4 code

---
+++ OOPSBannerUC4.java
@@ -0,0 +1,18 @@
+public class OOPSBannerUC4{
+    public static void main(String[] args) {
+
+        String[] lines = {
+            " ***     ***     *****   ***** ",
+            "*   *   *   *    *    *  *     ",
+            "*   *   *   *    *    *  *     ",
+            "*   *   *   *    *****   ***** ",
+            "*   *   *   *    *       *     ",
+            "*   *   *   *    *       *     ",
+            " ***     ***     *       ***** "
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
